e1791702e0ccc58d5b19b3e9b845a35b9c8b4de7
Om chaudhari <omchaudhari0003@gmail.com>
2024-03-24 14:08:50 +0530

    Admin Functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,9 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
-import  jwt  from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 
-export const signup = async (req, res ,next) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (
@@ -33,45 +33,49 @@ export const signup = async (req, res ,next) => {
   }
 };
 
-export const signin = async(req , res , next) =>{
-    const {email , password} = req.body;
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
 
-    if(!email || !password || email === '' || password === ''){
-      next(errorHandler(400 , 'All fields are required'))
+  if (!email || !password || email === '' || password === '') {
+    next(errorHandler(400, 'All fields are required'));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, 'User not found'));
     }
-
-    try {
-      const validUser = await User.findOne({ email })
-      if(!validUser){
-        return next(errorHandler(400, 'User Not Found'));
-      }
-      const validPassword = bcryptjs.compareSync(password, validUser.password);
-      if(!validPassword){
-        return next(errorHandler(400,'Invalid Password'));
-      }
-
-      const token = jwt.sign({id: validUser._id} , process.env.JWT_SECRET)
-
-      const {password : pass , ...rest} = validUser._doc;
-
-      res
-        .status(200)
-        .cookie('access_token',token , {
-          httpOnly: true})
-          .json(rest)      
-    } catch (error) {
-      next(error)
-      
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, 'Invalid password'));
     }
-}
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
 
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const google = async (req, res, next) => {
   const { email, name, googlePhotoUrl } = req.body;
   try {
     const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET
+      );
       const { password, ...rest } = user._doc;
       res
         .status(200)
@@ -93,7 +97,10 @@ export const google = async (req, res, next) => {
         profilePicture: googlePhotoUrl,
       });
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: newUser._id, isAdmin: newUser.isAdmin },
+        process.env.JWT_SECRET
+      );
       const { password, ...rest } = newUser._doc;
       res
         .status(200)
